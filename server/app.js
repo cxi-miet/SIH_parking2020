29858f6e490c3937213798ee7764177c9323ce12
Mohd Umar Alam <umaralam48@gmail.com>
2020-01-10 15:43:04 +0530

    Removed views

--- server/app.js
+++ server/app.js
@@ -24,10 +24,6 @@ connect.then(
 
 //const Dishes = require('./models/dishes');
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -51,7 +47,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.send("error");
 });
 
 module.exports = app;
