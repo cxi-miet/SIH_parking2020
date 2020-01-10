40dc1792915158cecb5e88e8aa062fe1120ce8b8
Mohd Umar Alam <umaralam48@gmail.com>
2020-01-10 15:48:18 +0530

    Fixed mongo connection

--- server/app.js
+++ server/app.js
@@ -11,18 +11,11 @@ var app = express();
 
 const mongoose = require("mongoose");
 
-const url = "mongodb://localhost:27017/parking";
-const connect = mongoose.connect(url);
-connect.then(
-  db => {
-    console.log("Connected correctly to server");
-  },
-  err => {
-    console.log(err);
-  }
-);
-
-//const Dishes = require('./models/dishes');
+var dev_db_url = "mongodb://127.0.0.1/parking";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use(logger("dev"));
 app.use(express.json());
