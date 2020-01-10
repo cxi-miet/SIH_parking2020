a60c2439e201b2a3299740e02a5970cf6b428029
Shristhi Pahwa <shristhi.pahwa24@gmail.com>
2020-01-10 15:47:48 +0530

    removed model

--- server/routes/residentRouter.js
+++ server/routes/residentRouter.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-//const Residents = require('../models/residents');
 
 const residentRouter = express.Router();
 
