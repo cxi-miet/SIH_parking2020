2dd0bb24f0bbd38e15013f9cc1d39811f35f1231
Shristhi Pahwa <shristhi.pahwa24@gmail.com>
2020-01-18 14:32:11 +0530

    added front-end

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var router = express.Router();
-
+var path = require("path");
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.send("index");
+  res.sendFile("register_form.html", {
+    root: path.join(__dirname, "../public")
+  });
 });
 
 module.exports = router;
